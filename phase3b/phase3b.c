fd1ffa4164dec2ddcdaef4a1a638385a300e6473
Bianca Ana Lara <balara@cs.arizona.edu>
2020-11-02 16:11:55 -0700

    added some starter code for phase3b

--- phase3b/phase3b.c
+++ phase3b/phase3b.c
@@ -15,6 +15,33 @@
 void
 P3PageFaultHandler(int type, void *arg)
 {
+    int offset = (int)(*arg);
+    int pid = P1_GetPid();
+    int pageSize = USLOSS_MmuPageSize();
+    int page = offset/pageSize;
+
+    if (type == USLOSS_MMU_FAULT) {
+      USLOSS_Console("PAGE FAULT!!! PID %d page %d\n", pid, page);
+      USLOSS_PTE* pageTable;
+      int ret = P3PageTableGet(pid, &pageTable);
+      if (ret != P1_SUCCESS || pageTable == NULL) {
+          USLOSS_Console("PAGE FAULT!!! PID %d has no page table!!!\n", pid);
+          USLOSS_Halt(1);
+      } else {
+        // determine which page suffered the fault (USLOSS_MmuPageSize)
+        //     update the page's PTE to map page x to frame x
+        //     set the PTE to be read-write and incore
+        //     update the page table in the MMU (USLOSS_MmuSetPageTable)
+      }
+    } else {
+      if (type == USLOSS_MMU_ACCESS) {
+        USLOSS_Console("PROTECTION VIOLATION!!! PID %d offset 0x%x!!!\n", pid, offset);
+      } else {
+        USLOSS_Console("Unknown cause: %d\n", cause);
+      }
+      USLOSS_Halt(1);
+    }
+
     /*******************
     get the offset from *arg
     get pid from P1_GetPid()
@@ -43,12 +70,22 @@ P3PageFaultHandler(int type, void *arg)
         USLOSS_Halt(1)
     *********************/
 
+
 }
 
 USLOSS_PTE *
 P3PageTableAllocateEmpty(int pages)
 {
     USLOSS_PTE  *table = NULL;
+    USLOSS_PTE *tail = table;
+    int i=0;
+    while (i < pages) {
+      USLOSS_PTE newEntry = malloc(sizeof(USLOSS_PTE));
+      newEntry.incore = 0;
+      *tail = newEntry;
+      tail++;
+    }
+
     // allocate and initialize an empty page table
     return table;
 }
